89e5cea837b87b3e45854ee5c25c2ffa346ccf09
rootellen <rootellen@users.noreply.github.com>
2020-12-15 09:02:24 -0300

    Update cart.js

--- js/cart.js
+++ js/cart.js
@@ -22,7 +22,7 @@ Array.prototype.forEach.call(buttons_add_to_cart, function(e) {
         });
 
         request.done(function (product) {
-            addNewItemInCart(product, qty);
+            addProductInCart(product, qty);
         });
 
         request.fail(function (jqXHR, textStatus) {
@@ -34,7 +34,11 @@ Array.prototype.forEach.call(buttons_add_to_cart, function(e) {
     })
 });
 
-function addNewItemInCart(product, qty) {
+function addProductInCart(product, qty) {
+    var productExists = false;
+    var cartList = $(".shopping-cart-list")[0];
+    var cartListCount = $(".shopping-cart-list")[0].childElementCount;
+
     var np = `
             <div class="product product-widget">
                 <div class="product-thumb">
@@ -47,31 +51,29 @@ function addNewItemInCart(product, qty) {
                 <button class="cancel-btn"><i class="fa fa-trash"></i></button>
             </div>
         `
-    var exists = false;
-    var cart_list = $(".shopping-cart-list")[0];
-    var cart_list_count = $(".shopping-cart-list")[0].childElementCount;
 
-    for (var index = 0; index < cart_list_count; index++) {
-        img = cart_list.children[index].childNodes[1].children[0].currentSrc
+    for (var index = 0; index < cartListCount; index++) {
+        img = cartList.children[index].childNodes[1].children[0].currentSrc
         if (img == product["imagem"]) {
-            exists = true;
-            $(cart_list.children[index]).replaceWith(np);
+            productExists = true;
+            $(cartList.children[index]).replaceWith(np);
             break;
         }
     }
+    
 
-    if (!exists) {
+    if (!productExists) {
         $(".shopping-cart-list").append(np);
     }
     
-    var qty_total = $("#qty_cart");
-    qty_total.html(parseInt(qty_total.html()) + parseInt(qty));
-    var preco_total = $('#carrinho-preco-total');
-    var preco_atual = (preco_total.html());
-    preco_atual = parseFloat(preco_atual.slice(1, preco_atual.length));
-    preco_soma = parseFloat(product["preco"]) * parseInt(qty);
-    var novo_preco = preco_atual + preco_soma;
-    novo_preco = novo_preco.toFixed(2);
-    preco_total.html("$" + novo_preco);
+    var qtdTotal = $("#qty_cart");
+    qtdTotal.html(parseInt(qtdTotal.html()) + parseInt(qty));
+    var precoTotal = $('#carrinho-preco-total');
+    var precoAtual = (precoTotal.html());
+    precoAtual = parseFloat(precoAtual.slice(1, precoAtual.length));
+    precoSoma = parseFloat(product["preco"]) * parseInt(qty);
+    var novoPreco = precoAtual + precoSoma;
+    novoPreco = novoPreco.toFixed(2);
+    precoTotal.html("$" + novoPreco);
 
 }
